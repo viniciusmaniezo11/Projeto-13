8d419762e438992f3770abaf81a1677ab7b175d1
Fabricio Honorato Gomes <55056188808@info.local>
2026-05-06 16:28:25 -0300

    exercicio 13

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,71 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Bounce2.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+uint8_t tela = 0;
+String resposta;
+
+void tela1();
+void tela2();
+void tela3();
+void atualizarTela();
+
+const char *URL_API = "https://api.weatherapi.com/v1/current.json?key=c11009a9d28749e6af4164128261604&q=Sao%20Caetano%20do%20Sul&lang=pt";
+
+Bounce botaoBoot = Bounce();
+
+void setup() 
+{
+  botaoBoot.attach(0, INPUT_PULLUP);
+  lcd.init();
+  lcd.backlight();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void AtualizarTela()
+{
+  switch (tela)
+  {
+  case 0:
+    tela1();
+    break;
+  case 1:
+    tela2();
+    break;
+  case 2:
+    tela3();
+    break;
+  }
+}
+
+void tela1()
+{
+   JsonDocument doc;
+   DeserializationError erro = deserializeJson(doc, resposta);
+
+  if(!erro)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Consultório disponível");
+
+    lcd.setCursor(0,2);
+    lcd.print("Doutor Thiago");
+
+    if(doc["location"]["localtime"].is<JsonVariant>())
+    {
+      const char *hora = doc["location"]["localtime"];
+      lcd.setCursor(0,3);
+      lcd.printf("%d:%d", hora);
+    }
+  }
 }
